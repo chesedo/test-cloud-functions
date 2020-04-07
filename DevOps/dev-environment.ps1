fbb9f28c44b20682daa05e9eff1111febde212f1
Pieter Engelbrecht <pengelbrecht@rems2.com>
2020-04-07 15:53:19 +0200

    Adds dev setup scripts

---
+++ DevOps/dev-environment.ps1
@@ -0,0 +1,5 @@
+# Install Python3
+choco install python3
+
+# Install Poetry
+(Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python
---
+++ DevOps/dev-environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Python 3.7
+apt install -y python3.7
+
+# Install Poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
