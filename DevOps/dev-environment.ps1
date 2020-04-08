0c42ced1da40fceaa86f3ea59d5d2418d5705d87
Pieter Engelbrecht <pengelbrecht@rems2.com>
2020-04-08 15:19:42 +0200

    Include extensions in dev files

--- DevOps/dev-environment.ps1
+++ DevOps/dev-environment.ps1
@@ -3,3 +3,23 @@ choco install python3
 
 # Install Poetry
 (Invoke-WebRequest -Uri https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py -UseBasicParsing).Content | python
+
+# Install VSCode
+choco install vscode
+
+# Install VSCode extensions
+## For python development
+
+### Python extension
+code --install-extension ms-python.python
+
+### Toml syntax extension
+code --install-extension bungcip.better-toml
+
+## General
+
+### Spelling
+code --install-extension ban.spellright
+
+### Api testing
+code --install-extension humao.rest-client
--- DevOps/dev-environment.sh
+++ DevOps/dev-environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Python 3.7
-apt install -y python3.7
+apt install -y python3.7 python3.7-distutils python3.7-venv
 
 # Install Poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
