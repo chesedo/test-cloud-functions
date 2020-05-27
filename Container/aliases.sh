a4c818f1c240f887371043d06b4b0f4f852db178
Pieter Engelbrecht <pengelbrecht@rems2.com>
2020-05-27 10:09:24 +0200

    Fix pipeline - part 1

--- Container/aliases.sh
+++ Container/aliases.sh
@@ -1,12 +1,4 @@
-# Development commands
-alias test="pytest --doctest-modules --doctest-continue-on-failure --ignore=main.py --cov $FUNCTION"
-alias lint_flake8="flake8"
-alias lint_mypy="mypy ."
-alias lint="lint_flake8 && lint_mypy"
-alias format="black --line-length 119 ."
-
 # Local debugging
-alias start="functions-framework --target $FUNCTION --signature-type event"
 alias devel="start --debug"
 
 # Deploy tasks
